c34335878f90f18488d7dfc6b75ec2186ac15a76
Fabian Naumiuk <fabian.naumiuk@gmail.com>
2023-02-07 11:55:52 +0100

    Add project files.

---
+++ P07Blazor.Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using P05Sklep.Shared;
+using System.Net.Http.Json;
+
+namespace P07Blazor.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<ServiceResponse<Product[]>> GetProducts()
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<Product[]>>("api/product");
+
+            return result;
+        }
+    }
+}
